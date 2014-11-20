51374ae5e3f0c18bf7a3c943a93c57d3e804c632
Ricardo Costa <canecao@gmail.com>
2014-11-20 18:15:10 -0200

    Classe Telefone
    Classe para interagir com o telefone android

---
+++ uTelefoneAndroid.pas
@@ -0,0 +1,36 @@
+unit uTelefoneAndroid;
+
+interface
+
+procedure EnviarSMS(pNumero, pMensagem: string);
+function PegaNumeroTelefone: String;
+
+implementation
+
+uses
+  Androidapi.JNI.JavaTypes,
+  Androidapi.JNI.Telephony,
+  Androidapi.Helpers,
+
+  Androidapi.JNIBridge,
+  Androidapi.JNI.GraphicsContentViewText;
+
+procedure EnviarSMS(pNumero, pMensagem: string);
+var
+  GerenciadorSMS: JSmsManager;
+begin
+  GerenciadorSMS := TJSmsManager.JavaClass.getDefault;
+  GerenciadorSMS.sendTextMessage(StringToJString(pNumero), nil, StringToJString(pMensagem), nil, nil)
+end;
+
+function PegaNumeroTelefone: String;
+var
+  TTelMgr: JTelephonyManager;
+  NumeroFone: String;
+begin
+  TTelMgr := TJTelephonyManager.Wrap ((SharedActivityContext.getSystemService(TJContext.JavaClass.TELEPHONY_SERVICE) as ILocalObject).GetObjectID);
+  NumeroFone := JStringToString(TTelMgr.getLine1Number);
+  Result := NumeroFone;
+end;
+
+end.
