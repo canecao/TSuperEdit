0625653ce874a7669b9b95cdd8440102b19b3cbe
Ricardo Costa <canecao@gmail.com>
2014-12-02 00:00:01 -0200

    ttt
    ttt

--- uFormatador.pas
+++ uFormatador.pas
@@ -2,209 +2,236 @@ unit uFormatador;
 
 interface
 
-   uses uTipos, System.Classes, FMX.Forms, FMX.Edit;
+uses uTipos, System.Classes, FMX.Forms, FMX.Edit;
 
-   type
-      TThreadTremer = class( TThread )
-      private
-         FForm: TForm;
-         FEdit: TEdit;
-         procedure SetEdit( const Value: TEdit );
-         procedure SetForm( const Value: TForm );
-      protected
-         procedure Execute; override;
-      public
-         constructor Create( Componente: TObject ); overload;
-         property Edit: TEdit read FEdit write SetEdit;
-         property Form: TForm read FForm write SetForm;
-      end;
+type
+  TThreadTremer = class(TThread)
+  private
+    FForm: TForm;
+    FEdit: TEdit;
+    procedure SetEdit(const Value: TEdit);
+    procedure SetForm(const Value: TForm);
+  protected
+    procedure Execute; override;
+  public
+    constructor Create(Componente: TObject); overload;
+    property Edit: TEdit read FEdit write SetEdit;
+    property Form: TForm read FForm write SetForm;
+  end;
 
-      TFormatador = class
-      public
-         function Mascarar( texto, mascara: String ): string;
-         function Limpar( pTexto: String ): String;
-         function Pad( pCaracter: char; pTamanho: integer ): String;
-         function RPad( pTexto: String; pCaracter: char; pTamanho: integer ): String;
-         function LPad( pTexto: String; pCaracter: char; pTamanho: integer ): String;
-         function ArrayToStr( Vetor: array of String; Delimitador: char = ',' ): String;
-         function Split( texto: String; Delimitador: char = ',' ): TStrings;
-         class function ColocaMascara( pTipoDocumento: TTipoDoc; pString: String ): String;
-         class function RetiraMascara( pString: String ): String;
-         Class procedure Tremer( Sender: TEdit ); overload;
-         Class procedure Tremer( Sender: TForm ); overload;
-         class function GetMascara( TipoDoc: TTipoDoc ): String;
-         property mascara[ TipoDoc: TTipoDoc ]: String read GetMascara;
-      end;
+  TFormatador = class
+  public
+    function Mascarar(texto, mascara: String): string;
+    function Limpar(pTexto: String): String;
+    function Pad(pCaracter: char; pTamanho: integer): String;
+    function RPad(pTexto: String; pCaracter: char; pTamanho: integer): String;
+    function LPad(pTexto: String; pCaracter: char; pTamanho: integer): String;
+    function ArrayToStr(Vetor: array of String;
+      Delimitador: char = ','): String;
+    function Split(texto: String; Delimitador: char = ','): TStrings;
+    class function ColocaMascara(pTipoDocumento: TTipoDoc;
+      pString: String): String;
+    class function RetiraMascara(pString: String): String;
+    Class procedure Tremer(Sender: TEdit); overload;
+    Class procedure Tremer(Sender: TForm); overload;
+    class function GetMascara(TipoDoc: TTipoDoc): String;
+    property mascara[TipoDoc: TTipoDoc]: String read GetMascara;
+  end;
 
 implementation
 
-   uses
-      Math, System.SysUtils, System.StrUtils;
+uses
+  Math, System.SysUtils, System.StrUtils;
 
-   { TFormatador }
-   function TFormatador.Split( texto: String; Delimitador: char = ',' ): TStrings;
-   begin
-      Result                 := TStringList.Create;
-      Result.Delimiter       := Delimitador;
-      Result.StrictDelimiter := True;
-      Result.DelimitedText   := texto;
-   end;
+{ TFormatador }
+function TFormatador.Split(texto: String; Delimitador: char = ','): TStrings;
+begin
+  Result := TStringList.Create;
+  Result.Delimiter := Delimitador;
+  Result.StrictDelimiter := True;
+  Result.DelimitedText := texto;
+end;
 
-function TFormatador.ArrayToStr( Vetor: array of String; Delimitador: char = ',' ): String;
-   var
-      I: integer;
-   begin
-      Result    := Vetor[ Low( Vetor ) ];
-      for I     := Low( Vetor ) + 1 to High( Vetor ) do
-         Result := Result + Delimitador + Vetor[ I ];
-   end;
+function TFormatador.ArrayToStr(Vetor: array of String;
+  Delimitador: char = ','): String;
+var
+  I: integer;
+begin
+  Result := Vetor[Low(Vetor)];
+  for I := Low(Vetor) + 1 to High(Vetor) do
+    Result := Result + Delimitador + Vetor[I];
+end;
 
-   function TFormatador.Pad( pCaracter: char; pTamanho: integer ): String;
-   begin
-      Result := StringOfChar( pCaracter, pTamanho );
-   end;
+function TFormatador.Pad(pCaracter: char; pTamanho: integer): String;
+begin
+  Result := StringOfChar(pCaracter, pTamanho);
+end;
 
-   function TFormatador.LPad( pTexto: String; pCaracter: char; pTamanho: integer ): String;
-   var
-      Quantidade: integer;
-   begin
-      Quantidade := pTamanho - Length( Trim( pTexto ) );
-      Result     := Pad( pCaracter, Quantidade ) + Trim( pTexto );
-   end;
+function TFormatador.LPad(pTexto: String; pCaracter: char;
+  pTamanho: integer): String;
+var
+  Quantidade: integer;
+begin
+  Quantidade := pTamanho - Length(Trim(pTexto));
+  Result := Pad(pCaracter, Quantidade) + Trim(pTexto);
+end;
 
-   function TFormatador.RPad( pTexto: String; pCaracter: char; pTamanho: integer ): String;
-   var
-      Quantidade: integer;
-   begin
-      Quantidade := pTamanho - Length( Trim( pTexto ) );
-      Result     := Trim( pTexto ) + Pad( pCaracter, Quantidade );
-   end;
+function TFormatador.RPad(pTexto: String; pCaracter: char;
+  pTamanho: integer): String;
+var
+  Quantidade: integer;
+begin
+  Quantidade := pTamanho - Length(Trim(pTexto));
+  Result := Trim(pTexto) + Pad(pCaracter, Quantidade);
+end;
 
-   class function TFormatador.GetMascara( TipoDoc: TTipoDoc ): String;
-   begin
-      case TipoDoc of
-         tpCPF: Result         := '999.999.999-99';
-         tpCNPJ: Result        := '99.999.999/9999-99';
-         tpData: Result        := '99-99-9999';
-         tpCep: Result         := '99.999-999';
-         tpCelular: Result     := '99999-9999';
-         tpTelefone: Result    := '9999-9999';
-         tpCelularDDD: Result  := '(99) 99999-9999';
-         tpTelefoneDDD: Result := '(99) 9999-9999';
+class function TFormatador.GetMascara(TipoDoc: TTipoDoc): String;
+begin
+  case TipoDoc of
+    tpCPF:
+      Result := '999.999.999-99';
+    tpCNPJ:
+      Result := '99.999.999/9999-99';
+    tpData:
+      Result := '99-99-9999';
+    tpCep:
+      Result := '99.999-999';
+    tpCelular:
+      Result := '99999-9999';
+    tpTelefone:
+      Result := '9999-9999';
+    tpCelularDDD:
+      Result := '(99)99999-9999';
+    tpTelefoneDDD:
+      Result := '(99)9999-9999';
+    tpMascaraRede:
+      Result := '999.999.999.999';
+  end;
+end;
+
+function TFormatador.Limpar(pTexto: String): String;
+var
+  Aux: Boolean;
+  C: char;
+begin
+  Result := '';
+  for C in pTexto do
+  begin
+    Aux := CharInSet(C, ['0' .. '9']);
+    Result := Result + ifthen(Aux, C, EmptyStr);
+  end;
+end;
+
+function TFormatador.Mascarar(texto: String; mascara: String): string;
+var
+  I, J: integer;
+  AuxEdt, AuxStr: Boolean;
+  C: char;
+begin
+  Result := EmptyStr;
+  if texto <> EmptyStr then
+  begin
+    // 1111111111111111
+    // (99)99999-9999
+    I := 0;
+    for C in mascara do
+    begin
+      AuxEdt := CharInSet(texto[i], ['0' .. '9' ]);
+      AuxStr := (c <> '9');
+      if texto[i] = #0 then
+         inc(i);
+      if AuxStr then
+      begin
+        Result := Result + c;
+      end else
+      if AuxEdt then
+      begin
+        Result := Result + texto[i];
+        inc(I);
       end;
-   end;
+    end;
+  end;
+end;
 
-   function TFormatador.Limpar( pTexto: String ): String;
-   var
-      Aux: Boolean;
-      C  : char;
-   begin
-      Result := '';
-      for C in pTexto do
-         begin
-            Aux    := CharInSet( C, [ '0' .. '9' ] );
-            Result := Result + ifthen( Aux, C, EmptyStr );
-         end;
-   end;
+class procedure TFormatador.Tremer(Sender: TForm);
+begin
+  TThreadTremer.Create(Sender);
+end;
 
-   function TFormatador.Mascarar( texto: String; mascara: String ): string;
-   var
-      I             : integer;
-      AuxEdt, AuxStr: Boolean;
-   begin
-      if texto <> EmptyStr then
-         begin
-            I := 1;
-            Repeat
-               AuxEdt := CharInSet( texto[ I ], [ '0' .. '9' ] );
-               AuxStr := mascara[ I ] = '9';
-               if AuxStr and not AuxEdt then
-                  delete( texto, I, 1 );
-               if not AuxStr and AuxEdt then
-                  insert( mascara[ I ], texto, I );
-               inc( I );
-            until ( I > Length( texto ) ) or ( I > Length( mascara ) );
-         end;
-      Result := ifthen( Length( texto ) <= Length( mascara ), texto, Copy( texto, 1, Length( mascara ) ) );
-   end;
+class procedure TFormatador.Tremer(Sender: TEdit);
+begin
+  TThreadTremer.Create(Sender);
+end;
 
-   class procedure TFormatador.Tremer( Sender: TForm );
-   begin
-      TThreadTremer.Create( Sender );
-   end;
+class function TFormatador.ColocaMascara(pTipoDocumento: TTipoDoc;
+  pString: String): String;
+var
+  Formatador: TFormatador;
+begin
+  Formatador := TFormatador.Create;
+  Result := Formatador.Mascarar(pString, Formatador.mascara[pTipoDocumento]);
+  FreeAndNil(Formatador);
+end;
 
-   class procedure TFormatador.Tremer( Sender: TEdit );
-   begin
-      TThreadTremer.Create( Sender );
-   end;
+class function TFormatador.RetiraMascara(pString: String): String;
+var
+  Formatador: TFormatador;
+begin
+  Formatador := TFormatador.Create;
+  Result := Formatador.Limpar(pString);
+  FreeAndNil(Formatador);
+end;
 
-   class function TFormatador.ColocaMascara( pTipoDocumento: TTipoDoc; pString: String ): String;
-   var
-      Formatador: TFormatador;
-   begin
-      Formatador := TFormatador.Create;
-      Result     := Formatador.Mascarar( pString, Formatador.mascara[ pTipoDocumento ] );
-      FreeAndNil( Formatador );
-   end;
+{ ObjetoTThread }
 
-   class function TFormatador.RetiraMascara( pString: String ): String;
-   var
-      Formatador: TFormatador;
-   begin
-      Formatador := TFormatador.Create;
-      Result     := Formatador.Limpar( pString );
-      FreeAndNil( Formatador );
-   end;
+procedure TThreadTremer.Execute;
+var
+  X: Single;
+  C: integer;
+begin
+  if Assigned(Edit) then
+  begin
+    X := Self.Edit.Position.X;
+    for C := 1 to 9 do
+    begin
+      Self.Edit.Position.X := Self.Edit.Position.X +
+        ifthen(Self.Edit.Position.X >= X, -15, 30);
+      Sleep(50);
+      Application.ProcessMessages;
+    end;
+  end;
+  if Assigned(Form) then
+  begin
+    X := Self.Form.Left;
+    for C := 1 to 9 do
+    begin
+      Self.Form.Left := Self.Form.Left + ifthen(Self.Form.Left >= X, -15, 30);
+      Sleep(50);
+      Application.ProcessMessages;
+    end;
+  end;
+end;
 
-   { ObjetoTThread }
+procedure TThreadTremer.SetEdit(const Value: TEdit);
+begin
+  FEdit := Value;
+end;
 
-   procedure TThreadTremer.Execute;
-   var
-      X: Single;
-      C: integer;
-   begin
-      if Assigned( Edit ) then
-         begin
-            X     := Self.Edit.Position.X;
-            for C := 1 to 9 do
-               begin
-                  Self.Edit.Position.X := Self.Edit.Position.X + ifthen( Self.Edit.Position.X >= X, - 15, 30 );
-                  Sleep( 50 );
-                  Application.ProcessMessages;
-               end;
-         end;
-      if Assigned( Form ) then
-         begin
-            X     := Self.Form.Left;
-            for C := 1 to 9 do
-               begin
-                  Self.Form.Left := Self.Form.Left + ifthen( Self.Form.Left >= X, - 15, 30 );
-                  Sleep( 50 );
-                  Application.ProcessMessages;
-               end;
-         end;
-   end;
+procedure TThreadTremer.SetForm(const Value: TForm);
+begin
+  FForm := Value;
+end;
 
-   procedure TThreadTremer.SetEdit( const Value: TEdit );
-   begin
-      FEdit := Value;
-   end;
-
-   procedure TThreadTremer.SetForm( const Value: TForm );
-   begin
-      FForm := Value;
-   end;
-
-   constructor TThreadTremer.Create( Componente: TObject );
-   begin
-      // let thread free itself
-      FreeOnTerminate := True;
-      // do not create suspended, let it go as soon as it is created
-      if Componente.ClassParent = TEdit then
-         Self.Edit := TEdit( Componente )
-      else
-         Self.Form := TForm( Componente );
-      inherited Create( False );
-   end;
+constructor TThreadTremer.Create(Componente: TObject);
+begin
+  // let thread free itself
+  FreeOnTerminate := True;
+  // do not create suspended, let it go as soon as it is created
+  if Componente.ClassParent = TEdit then
+    Self.Edit := TEdit(Componente)
+  else
+    Self.Form := TForm(Componente);
+  inherited Create(False);
+end;
 
 end.
--- uTelefoneAndroid.pas
+++ uTelefoneAndroid.pas
@@ -2,33 +2,69 @@ unit uTelefoneAndroid;
 
 interface
 
+
 procedure EnviarSMS(pNumero, pMensagem: string);
 function PegaNumeroTelefone: String;
-
+procedure EnviarNotificacao(pNome, pCorpo : String; pHora : TTime);
 implementation
-
+ {$IFDEF ANDROID}
 uses
+
   Androidapi.JNI.JavaTypes,
   Androidapi.JNI.Telephony,
   Androidapi.Helpers,
 
   Androidapi.JNIBridge,
-  Androidapi.JNI.GraphicsContentViewText;
-
+  Androidapi.JNI.GraphicsContentViewText, FMX.Notification;
+ {$ENDIF}
 procedure EnviarSMS(pNumero, pMensagem: string);
+ {$IFDEF ANDROID}
 var
-  GerenciadorSMS: JSmsManager;
+  GerenciadorSMS: JSmsManager; {$ENDIF}
 begin
+ {$IFDEF ANDROID}
   GerenciadorSMS := TJSmsManager.JavaClass.getDefault;
   GerenciadorSMS.sendTextMessage(StringToJString(pNumero), nil, StringToJString(pMensagem), nil, nil)
+{$ENDIF}
 end;
 
 function PegaNumeroTelefone: String;
+ {$IFDEF ANDROID}
 var
-  TTelMgr: JTelephonyManager;
+  TTelMgr: JTelephonyManager;   {$ENDIF}
 begin
+ {$IFDEF ANDROID}
   TTelMgr := TJTelephonyManager.Wrap ((SharedActivityContext.getSystemService(TJContext.JavaClass.TELEPHONY_SERVICE) as ILocalObject).GetObjectID);
   Result := JStringToString(TTelMgr.getLine1Number);
+ {$ENDIF}
+ end;
+
+procedure EnviarNotificacao(pNome, pCorpo : String; pHora : TTime);
+ {$IFDEF ANDROID}
+var   Notification: TNotification;
+      NotificationC : TNotificationCenter;  {$ENDIF}
+begin
+  {$IFDEF ANDROID}
+  { verify if the service is actually supported }
+  NotificationC := TNotificationCenter.Create(nil);
+  if NotificationC.Supported then
+  begin
+    Notification := NotificationC.CreateNotification;
+    try
+      Notification.Name := pNome;
+      Notification.AlertBody := pCorpo;
+      Notification.FireDate := pHora;
+
+      { Send notification in Notification Center }
+      NotificationC.ScheduleNotification(Notification);
+      { also this method is equivalent }
+      // NotificationService.PresentNotification(Notification);
+    finally
+      Notification.DisposeOf;
+      NotificationC.DisposeOf;
+    end;
+  end;               {$ENDIF}
 end;
 
 end.
+
